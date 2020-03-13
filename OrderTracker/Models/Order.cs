0cf06c1f89e49575fbcb4ec05a25068aae371b61
Dusty <dustymccord@gmail.com>
2020-03-13 09:53:47 -0700

    add Order model and constructor.

--- OrderTracker/Models/Order.cs
+++ OrderTracker/Models/Order.cs
@@ -4,10 +4,28 @@ namespace OrderTracker.Models
 {
   class Order
   {
+    public static List<Order> MaserOrderList { get; set; } = new List<Order>(5);
+    public int IdCounter = 0;
 
-    Order()
+    public string ProductName { get; set; }
+    public int Quantity { get; set; }
+    public string DeliverBy { get; set; }
+    public int Id { get; set; }
+    public int VenderId { get; set; }
+
+    public string VenderName { get; set; }
+
+
+    Order(string productName, int quantity, string deliverBy, int venderId, string venderName)
     {
+      ProductName = productName;
+      Quantity = quantity;
+      DeliverBy = deliverBy;
+      VenderId = venderId;
+      VenderName = venderName;
+      Id = IdCounter++;
 
+      MaserOrderList.Add(this);
     }
 
   }

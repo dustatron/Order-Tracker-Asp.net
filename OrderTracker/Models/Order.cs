78de64516fd377438f2aeaa78d93e74e20083607
Dusty <dustymccord@gmail.com>
2020-03-13 10:11:02 -0700

    add crud functions to models for Orders and Venders.

--- OrderTracker/Models/Order.cs
+++ OrderTracker/Models/Order.cs
@@ -4,7 +4,7 @@ namespace OrderTracker.Models
 {
   class Order
   {
-    public static List<Order> MaserOrderList { get; set; } = new List<Order>(5);
+    public static List<Order> MasterOrderList { get; set; } = new List<Order>(5);
     public int IdCounter = 0;
 
     public string ProductName { get; set; }
@@ -25,7 +25,39 @@ namespace OrderTracker.Models
       VenderName = venderName;
       Id = IdCounter++;
 
-      MaserOrderList.Add(this);
+      MasterOrderList.Add(this);
+    }
+
+    public static List<Order> GetAll()
+    {
+      return MasterOrderList;
+    }
+
+    public static Order GetOrder(int id)
+    {
+      foreach (var order in MasterOrderList)
+      {
+        if (id == order.Id)
+        {
+          return order;
+        }
+      }
+      return MasterOrderList[0];
+    }
+
+    public static void Update(int id, string productName, int quantity, string deliverBy)
+    {
+      Order thisOrder = GetOrder(id);
+      thisOrder.ProductName = productName;
+      thisOrder.Quantity = quantity;
+      thisOrder.DeliverBy = deliverBy;
+
+    }
+
+    public static void Delete(int id)
+    {
+      Order thisOrder = GetOrder(id);
+      MasterOrderList.Remove(thisOrder);
     }
 
   }
--- OrderTracker/Models/Vender.cs
+++ OrderTracker/Models/Vender.cs
@@ -4,8 +4,8 @@ namespace OrderTracker.Models
 {
   class Vender
   {
-    public int IdCounter = 0;
-    private List<Vender> _vendersList = new List<Vender>(5);
+    public static int IdCounter = 0;
+    private static List<Vender> _vendersList = new List<Vender>(5);
     public string VenderName { get; set; }
     public string Address { get; set; }
     public string Description { get; set; }
@@ -23,6 +23,38 @@ namespace OrderTracker.Models
 
       _vendersList.Add(this);
     }
+
+    public static List<Vender> GetAllVenders()
+    {
+      return _vendersList;
+    }
+
+    public static Vender GetVender(int id)
+    {
+      foreach (var vender in _vendersList)
+      {
+        if (id == vender.Id)
+        {
+          return vender;
+        }
+      }
+      return _vendersList[0];
+    }
+
+    public static void Update(int id, string name, string address, string decription)
+    {
+      Vender thisVender = GetVender(id);
+      thisVender.VenderName = name;
+      thisVender.Address = address;
+      thisVender.Description = decription;
+    }
+
+    public static void Delete(int id)
+    {
+      Vender thisVender = GetVender(id);
+      _vendersList.Remove(thisVender);
+    }
+
   }
 
 
